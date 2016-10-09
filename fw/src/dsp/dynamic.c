e9c1ac0f11cb5712e81b4c6236e44255e8e1f1b1
Massimo Pennazio <massimo.pennazio@gmail.com>
2016-10-09 14:46:44 +0200

    Finished rms compressor algorithm tested with matlab

--- fw/src/dsp/dynamic.c
+++ fw/src/dsp/dynamic.c
@@ -8,110 +8,84 @@
 
 #include "dynamic.h"
 
+
 void dynamicProcess(const FloatAudioBuffer* restrict in, FloatAudioBuffer* restrict out,
         compressor_t* c)
 {
-	float thr = c->threshold;
-	float ratio = c->ratio; 
-
-	float gain_dB = 1.0f/ratio;
-  float gain_l = DB_TO_LINEAR(gain_dB); 
 	
+	float attTime = c->attack*1e-3; // From ms to sec
+	float relTime = c->decay*1e-3;
 
+	float sampleRate = CODEC_SAMPLERATE;
+	
+	float noise_thr_dB = -40.0;
+	float noise_thr_l = DB_TO_LINEAR(noise_thr_dB);
+
+	float ga = exp(-1.0f/(sampleRate*attTime));
+	float gr = exp(-1.0f/(sampleRate*relTime));	
+	
+	float envIn = 0.0f;
+	float envIn_r = 0.0f;
+	float envIn_l = 0.0f;
+	float out_r = 0.0f;
+	float out_l = 0.0f;
+	float envRms = 0.0f;
+	static float envOut = 0.0f;
+	float env = 0.0f;
+
+	float thr_l = DB_TO_LINEAR(c->threshold);
+	float ratio = c->ratio;
+	float cmpcoeff = 0.0f;
+	float cmpgain = 1.0f;
+	float postgain = DB_TO_LINEAR(c->postgain);
+	
+	if(ratio > 1)
+		cmpcoeff = 1.0 - (1.0/ratio);
+	else
+		cmpcoeff = 0.0;
+		
   for (unsigned s = 0; s < CODEC_SAMPLES_PER_FRAME; s++) {
 
-		if(in->s[s][0] <= thr)
-		{
-			out->s[s][0] = in->s[s][0];
-			out->s[s][1] = in->s[s][1];
-		}
-		else
-		{
-    	out->s[s][0] = gain_l * in->s[s][0];
-			out->s[s][1] = gain_l * in->s[s][1];
-		}
+		// Peak envelope detector
+		/*envIn_r = fabsf(in->s[s][0]);
+		envIn_l = fabsf(in->s[s][1]);
+
+		envIn = fmaxf(envIn_r, envIn_l);
+
+    if( envOut < envIn )
+        envOut = envIn + ga * (envOut - envIn);
+    else
+        envOut = envIn + gr * (envOut - envIn);*/
+
+		// Rms envelope detector
+		envIn_r = fabsf(in->s[s][0]);
+		envIn_l = fabsf(in->s[s][1]);
+
+		envIn = fmaxf(envIn_r, envIn_l);
+		envIn = envIn * envIn; 
+		
+		if( envOut < envIn )
+        envOut = envIn + ga * (envOut - envIn);
+    else
+        envOut = envIn + gr * (envOut - envIn);
+		 
+		envRms = sqrtf(envOut);
+		
+		env = envRms;
+		
+		if(env < noise_thr_l)
+			env = noise_thr_l;
+
+		// Compressor calculations
+		cmpgain = pow((thr_l/env), cmpcoeff);
+		if(cmpgain > 1.0f)
+			cmpgain = 1.0f;
+		
+		out_r = in->s[s][0] * cmpgain;
+		out_l = in->s[s][1] * cmpgain;
+
+		// Makeup gain
+		out->s[s][0] = out_r * postgain;
+		out->s[s][1] = out_l * postgain;
   }
-
 }
-
-/*
-
-Put here examples code found on the net to be cleared when finished
-
-void compress
-    (
-        float*  wav_in,     // signal
-        int     n,          // N samples
-        double  threshold,  // threshold (percents)
-        double  slope,      // slope angle (percents)
-        int     sr,         // sample rate (smp/sec)
-        double  tla,        // lookahead  (ms)
-        double  twnd,       // window time (ms)
-        double  tatt,       // attack time  (ms)
-        double  trel        // release time (ms)
-    )
-{
-    typedef float   stereodata[2];
-    stereodata*     wav = (stereodata*) wav_in; // our stereo signal
-    threshold *= 0.01;          // threshold to unity (0...1)
-    slope *= 0.01;              // slope to unity
-    tla *= 1e-3;                // lookahead time to seconds
-    twnd *= 1e-3;               // window time to seconds
-    tatt *= 1e-3;               // attack time to seconds
-    trel *= 1e-3;               // release time to seconds
-
-    // attack and release "per sample decay"
-    double  att = (tatt == 0.0) ? (0.0) : exp (-1.0 / (sr * tatt));
-    double  rel = (trel == 0.0) ? (0.0) : exp (-1.0 / (sr * trel));
-
-    // envelope
-    double  env = 0.0;
-
-    // sample offset to lookahead wnd start
-    int     lhsmp = (int) (sr * tla);
-
-    // samples count in lookahead window
-    int     nrms = (int) (sr * twnd);
-
-    // for each sample...
-    for (int i = 0; i < n; ++i)
-    {
-        // now compute RMS
-        double  summ = 0;
-
-        // for each sample in window
-        for (int j = 0; j < nrms; ++j)
-        {
-            int     lki = i + j + lhsmp;
-            double  smp;
-
-            // if we in bounds of signal?
-            // if so, convert to mono
-            if (lki < n)
-                smp = 0.5 * wav[lki][0] + 0.5 * wav[lki][1];
-            else
-                smp = 0.0;      // if we out of bounds we just get zero in smp
-
-            summ += smp * smp;  // square em..
-        }
-
-        double  rms = sqrt (summ / nrms);   // root-mean-square
-
-        // dynamic selection: attack or release?
-        double  theta = rms > env ? att : rel;
-
-        // smoothing with capacitor, envelope extraction...
-        // here be aware of pIV denormal numbers glitch
-        env = (1.0 - theta) * rms + theta * env;
-
-        // the very easy hard knee 1:N compressor
-        double  gain = 1.0;
-        if (env > threshold)
-            gain = gain - (env - threshold) * slope;
-
-        // result - two hard kneed compressed channels...
-        float  leftchannel = wav[i][0] * gain;
-        float  rightchannel = wav[i][1] * gain;
-    }
-}
-*/
